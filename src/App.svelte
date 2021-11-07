2787bcde3a7f64d549cede3c0749dbaa778c941a
Lawrence Wang <lawrence.g.wang@gmail.com>
2021-11-06 19:12:43 -0700

    Make final adjustments for Citrus Hack submission

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,25 @@
 <script>
 	import {onMount} from 'svelte';
-	import Chart from 'chart.js/auto'
-	import * as graphs from 'chartjs-chart-graph';
-	import AutoComplete from "simple-svelte-autocomplete";
-	import ChartDataLabels from 'chartjs-plugin-datalabels';
+	import Chart from 'chart.js/auto' // https://www.chartjs.org/
+	import * as graphs from 'chartjs-chart-graph'; // https://github.com/sgratzl/chartjs-chart-graph
+	import AutoComplete from "simple-svelte-autocomplete"; // https://github.com/pstanoev/simple-svelte-autocomplete
+	import ChartDataLabels from 'chartjs-plugin-datalabels'; //https://github.com/chartjs/chartjs-plugin-datalabels
 
-	let canvas;
-	function createChart(nodes, edges, id, type, orientation) {
+	let loaded = false;
+	let canvas; // canvas that chart.js uses to draw
+	let requisites = []; 
+	let courses = [];
+	let selectedCourse;
+
+	function createChart(nodes, edges) {
 		if (canvas)
 			canvas.destroy();
-		canvas = new Chart(document.getElementById(id).getContext("2d"), {
-			type,
+		canvas = new Chart(document.getElementById("canvas").getContext("2d"), {
+			type: "dendogram",
 			data: {
 				labels: nodes.map((d) => d.name),
 				datasets: [{
-					pointBackgroundColor: 'red',
+					pointBackgroundColor: `#${Math.floor(Math.random()*16777215).toString(16)}`,
 					pointRadius: 20,
 					pointHoverRadius: 25,
 					data: nodes.map((d) => Object.assign({}, d)),
@@ -23,13 +28,13 @@
 			},
 			plugins: [ChartDataLabels],
 			options: {
-				tree: { orientation},
+				tree: { orientation: "vertical" },
 				layout: { padding: { left: 5, top: 5, bottom: 5, right: 5 } },
 				plugins: {
 					legend: { display: false },
 					tooltip: { enabled: false },
 					datalabels: {
-						color: 'blue',
+						color: `#${Math.floor(Math.random()*16777215).toString(16)}`,
 						formatter: function(value, context) { return value.name; },
 						align: 'bottom',
 						offset: 25,
@@ -40,16 +45,12 @@
 		});
 	}
 
-	// function randRange(min, max)
-	// {
-	// 	return (Math.random() * (max - min) + min)
-	// }
+	function randRange(min, max)
+	{
+		return Math.random() * (max - min) + min;
+	}
 
-	let requisites = [];
-	let courses = [];
-	let selectedCourse;
 	$: selectedCourse, updateMap();
-
 	function updateMap() {
 		if (requisites.length < 1)
 			return
@@ -60,14 +61,17 @@
 			prereq_dict[Object.keys(prereq_dict)[0]].forEach(prereq => {
 				const angle = Math.random()*Math.PI*2;
 				edges.push({ "source": 0, "target": nodes.length });
-				nodes.push({ "name":prereq, "x":Math.cos(angle)*0.7, "y":Math.sin(angle)*0.7 });
-				// nodes.push({ "name":prereq, "x":randRange(-0.8, 0.8), "y":randRange(-0.8, 0.8) });
+				// nodes.push({ "name":prereq, "x":Math.cos(angle)*0.85, "y":Math.sin(angle)*0.85 });
+				let x = 0, y = 0;
+				while (Math.abs(x) < 0.2 && Math.abs(y) < 0.2)
+				{
+					x = randRange(-0.8, 0.8);
+					y = randRange(-0.8, 0.8);
+				}
+				nodes.push({ "name": prereq, "x": x, "y": y });
 			})
 		})
-		console.log(nodes);
-		console.log(edges);
-		// createChart(requisites.nodes, requisites.edges, 'dh', 'dendogram', 'horizontal');
-		createChart(nodes, edges, 'dh', 'dendogram', 'vertical');
+		createChart(nodes, edges);
 	}
 
 	onMount(async () => {
@@ -76,16 +80,20 @@
 		.then(jsondata => {
 			requisites = jsondata;
 			courses = Object.keys(jsondata);
-			selectedCourse = courses[Math.floor(Math.random()*courses.length)];
-			console.log(requisites)
-			updateMap();
+			selectedCourse = courses[0]
+			while (requisites[selectedCourse].prerequisites.length < 2) // guarantee first map looks interesting
+				selectedCourse = courses[Math.floor(Math.random()*courses.length)];
+			loaded = true;
 		});
 	});
 </script>
 
 <main>
-	<AutoComplete items={courses} bind:selectedItem={selectedCourse} maxItemsToShowInList={3}/>
-	<canvas id="dh"></canvas>
+	{#if loaded}
+	<h1>Choose a course: </h1>
+	<AutoComplete items={courses} bind:selectedItem={selectedCourse} maxItemsToShowInList={5}/>
+	{/if}
+	<canvas id="canvas"></canvas>
 </main>
 
 <style>
@@ -93,9 +101,16 @@
 		text-align: center;
 		padding: 0;
 		width: 100%;
-		height: 100%;
+		height: 95%;
 		margin: 0 0;
-		overflow: hidden;
+	}
+
+	h1 {
+		display: inline;
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 1.5em;
+		font-weight: 100;
 	}
 
 	canvas {
